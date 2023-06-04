2be624807fd502957157bf9c61c2a387fe2c3e10
JousenD <romero87_2112@hotmail.com>
2023-06-04 18:54:39 +0200

    7. Initial Section Finished

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    [Header("Move Info")]
     public float moveSpeed;
     public float jumpForce;
+    private bool runBegun;
 
-    public Rigidbody2D rb;
+    [Header("Collision Info")]
+    public float groundCheckDistance;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +26,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        if (runBegun)
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        
+        CheckCollision();
+        CheckInput();
+    }
 
-        if (Input.GetButtonDown("Jump"))
-        {
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetButtonDown("Fire2"))
+            runBegun = true;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
